47463b16b35563627775be3786baed6e6da6787f
Dave McCormick <mccormick.david2@gmail.com>
2016-04-09 15:58:53 +1000

    adding file handler modules and time window for epoch calculation class

---
+++ src/pyAnimalTrack/backend/filehandlers/input_data.py
@@ -0,0 +1,20 @@
+
+class InputData(object):
+    """ Base input class to inherit from. Methods are all effectively pure virtual.
+    """
+
+    def getData(self):
+    	""" Source the data and return an object with accessible attributes.
+    	"""
+
+    	raise Exception("Child class must implement this method.")
+
+
+    def getColumn(self, columnName):
+    	""" Get a column of data from within the object.
+    		Object should be able to access a columnName of data as an attribute.
+    	"""
+
+    	raise Exception("Child class must implement this method.")
+
+
---
+++ src/pyAnimalTrack/backend/filehandlers/sensor_csv.py
@@ -0,0 +1,52 @@
+from input_data import InputData
+
+import pandas as pd
+
+class SensorCSV(InputData)
+
+	def __init__(self, filename):
+
+		        """ Constructor
+        
+            :param data: Initial input data
+            :returns: void
+        """
+        super(InputData,self).__init__()
+
+        self.__filename = filename
+        self.__df = None
+        self.__names =['ms','ax','ay','az','mx','my','mz','gx','gy','gz','temp','adjms']
+
+        return
+
+
+    def getData(self):
+    	""" Get an object representation of the censor CSV.
+
+  			:returns: A Pandas dataframe object.
+
+    	"""
+
+		self.__df = pd.read_csv(self.__filename, delimiter=";", names=self.__names)
+
+    	return self.__df
+
+
+    def getColumn(self, columnName):
+    	""" Gets a column of data.
+    		:param columnName: The name of the column to retreive.
+    		:returns: A numpy array of data for processing.
+    	"""
+
+    	return getattr(self.__df,columnName).values[::-1]
+
+
+    def getColumns(self):
+    	""" Lists all available column names.
+    		
+    		:returns: A list of strings representing the available column names.
+    	"""
+    	
+    	return self.__names
+
+
