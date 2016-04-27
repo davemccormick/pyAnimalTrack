b4836588e25f8622f6bb7d532cc8285a55171660
Dave McCormick <mccormick.david2@gmail.com>
2016-04-27 21:09:36 +1000

    bug fixes

--- src/pyAnimalTrack/backend/utilities/calculate_features.py
+++ src/pyAnimalTrack/backend/utilities/calculate_features.py
@@ -7,7 +7,7 @@ class CalculateFeatures(object):
     """
 
     @staticmethod
-    def calculate_sma(self, x, y, z):
+    def calculate_sma(x, y, z):
         """ Calculate the Signal Magnitude Area (SMA).
             This can be used to distuingish between periods of activity vs rest.
             Mathematical Notation: abs(Xi)+abs(Yi)+abs(Zi)
@@ -20,11 +20,11 @@ class CalculateFeatures(object):
 
         """
 
-        return np.add(np.absolute(x) + np.absolute(y) + np.absolute(z))
+        return np.absolute(x) + np.absolute(y) + np.absolute(z)
 
 
     @staticmethod
-    def calculate_svm(self, x, y, z):
+    def calculate_svm(x, y, z):
         """ Calculate the Signal Vector Magnitude (SVM).
             This indicates degree of movement intensity.
             Mathematical Notation: sqrt(Xi^2+Yi^2+Zi^2)
@@ -41,7 +41,7 @@ class CalculateFeatures(object):
 
 
     @staticmethod
-    def calculate_movement_variation(self, x, y, z):
+    def calculate_movement_variation(x, y, z):
         """ Calculate the movement_variation.
             Mathematical Notation: abs(Xi+1-Xi)+abs(Yi+1-Yi)+abs(Zi+1-Zi)
 
@@ -53,11 +53,11 @@ class CalculateFeatures(object):
 
         """
 
-        return np.add(np.absolute(np.add(x, np.subtract(1-x))) + np.absolute(np.add(y, np.subtract(1-y))) + np.absolute(np.add(z, np.subtract(1-z))))
+        return np.absolute(np.diff(x)) + np.absolute(np.diff(y)) + np.absolute(np.diff(z))
 
 
     @staticmethod
-    def calculate_energy(self, x, y, z):
+    def calculate_energy(x, y, z):
         """ Calculate the Energy of the combined axes.
             Mathematical Notation: (Xi^2+Yi^2+Zi^2)^2
 
@@ -69,11 +69,11 @@ class CalculateFeatures(object):
 
         """
 
-        return np.square(np.add(np.square(x) + np.square(y) + np.square(z)))
+        return np.square(np.square(x) + np.square(y) + np.square(z))
 
 
     @staticmethod
-    def calculate_entropy(self, x, y, z):
+    def calculate_entropy(x, y, z):
         """ Calculate the Entropy of the combined axes.
             Mathematical Notation: (1+(Xi+Yi+Zi))2*ln(1+(Xi+Yi+Zi))2
 
@@ -85,11 +85,11 @@ class CalculateFeatures(object):
 
         """
 
-        return np.square(np.add(1, np.add(x + y + z))) * np.log(np.square(np.add(1, np.add(x + y + z))))
+        return np.square(1 + (x + y + z)) * np.log(np.square(1 + (x + y + z)))
 
 
     @staticmethod
-    def calculate_pitch(self, x, y, z):
+    def calculate_pitch(x, y, z):
         """ Calculate the pitch in degrees of the combined axes.
             Mathematical Notation: tan^-1(-Xi/(sqrt(Yi+Zi)))*180/pi
 
@@ -101,11 +101,11 @@ class CalculateFeatures(object):
 
         """
 
-        return (np.tan(np.divide((-1 * x), np.sqrt(y+z))**1) * 180 / np.pi)
+        return (np.tan(np.divide((-x), np.sqrt(y**2 + z**2))**-1) * 180 / np.pi)
 
     
     @staticmethod
-    def calculate_roll(self, y, z):
+    def calculate_roll(y, z):
         """ Calculate the roll in degrees of the combined axes.
             Mathematical Notation: atan2(Yi,Zi)*180/pi
 
@@ -120,7 +120,7 @@ class CalculateFeatures(object):
 
     
     @staticmethod
-    def calculate_inclination(self, x, y, z):
+    def calculate_inclination(x, y, z):
         """ Calculate the inclination in degrees of the combined axes.
             Mathematical Notation: tan^-1((sqrt(Xi^2+Yi^2))/Zi)*180/pi
 
@@ -132,5 +132,5 @@ class CalculateFeatures(object):
 
         """
 
-        return (np.tan(np.divide((np.sqrt(np.square(x)+np.square(y)) / z) ** 1) * 180 / np.pi))
+        return (np.tan(np.divide((np.sqrt(x**2 + y**2)), z) **-1)) * 180 / np.pi
 
